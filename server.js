c7d37676e97f9ee702ba1b8dd0e73edbee5ad54b
Jason Cruz <mrcruz86@gmail.com>
2014-11-25 13:25:45 -0800

    changed port for aws

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-server.listen(3000, function () {
+server.listen(8000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
